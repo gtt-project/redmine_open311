ae9bda8b49a2eb58f1469a2461e2b6bbe167371b
Daniel Kastl <daniel@georepublic.de>
2016-10-18 13:46:44 +0900

    Adds plugin desctiption and metadata

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_open311 do
   name 'Redmine Open311 plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Georepublic'
+  description 'This is a plugin for adding Open311 API to Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  # url 'http://example.com/path/to/plugin'
+  author_url 'https://georepublic.info'
 end
